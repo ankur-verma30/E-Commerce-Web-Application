244dc2cadcbaa29179a0626757cca461c4157236
Ankur Verma <ankurverma7619@gmail.com>
2025-01-11 21:57:41 +0530

    Setuping the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
