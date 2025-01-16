20f6deb7950d35b35a8a6ed289105475d309fe0a
Ankur Verma <ankurverma7619@gmail.com>
2025-01-17 00:36:21 +0530

    working on front page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Hero from '../Components/Hero'
+import LatestCollections from '../Components/LatestCollections'
+import BestSeller from '../Components/BestSeller'
 
 const Home = () => {
   return (
     <div>
-        
+        <Hero/>
+        <LatestCollections/>
+        <BestSeller/>
     </div>
   )
 }
